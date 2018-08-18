098d4fe3c123dfa0dcdf4258a54ec0dbf6075058
wghxjl114 <243902219@qq.com>
2018-08-18 17:05:57 +0800

    new project

---
+++ CH11/CH11/Cards.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace CH11
+{
+    public class Cards: CollectionBase
+    {
+        public void Add(Card newCard)
+        {
+            List.Add(newCard);
+        }
+        public void Remove(Card oldCard)
+        {
+            List.Remove(oldCard);
+        }
+        public Card this[int cardIndex]
+        {
+            get { return (Card)List[cardIndex]; }
+            set { List[cardIndex] = value; }
+        }
+        public void CopyTo(Cards targetCards)
+        {
+            for (int index = 0; index < this.Count; index++)
+            {
+                targetCards[index] = this[index];
+            }
+        }
+        public bool Contains(Card card) => InnerList.Contains(card);
+    }
+}
---
+++ CH11/CH11/Suit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CH11
+{
+    public enum Suit
+    {
+        Club,
+        Diamond,
+        Spade,
+        Heart
+    }
+}
