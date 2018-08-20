af9a43a942f7329beaae65170eef4eca4dd3d643
wghxjl114 <243902219@qq.com>
2018-08-20 11:12:49 +0800

    project update

---
+++ Cards/CardsGUI/Options.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace CardsGUI
+{
+    /// <summary>
+    /// Options.xaml 的交互逻辑
+    /// </summary>
+    public partial class Options : Window
+    {
+        public Options()
+        {
+            if(_gameOptions==null)
+            {
+                if (File.Exists("GameOptions.xml"))
+                {
+                    using (var stream = File.OpenRead("GameOptions.xml"))
+                    {
+                        var serializer = new XmlSerializer(typeof(GameOptions));
+                        _gameOptions = serializer.Deserialize(stream) as GameOptions;
+                    }
+                }
+                else
+                    _gameOptions = new GameOptions();
+            }
+            InitializeComponent();
+            
+        }
+        private GameOptions _gameOptions;
+
+        private void dumbRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            _gameOptions.ComputerSkill = ComputerSkillLevel.Dumb;
+        }
+
+        private void goodRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            _gameOptions.ComputerSkill = ComputerSkillLevel.Good;
+        }
+
+        private void cheatingRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            _gameOptions.ComputerSkill = ComputerSkillLevel.Cheats;
+        }
+
+        
+    }
+}
